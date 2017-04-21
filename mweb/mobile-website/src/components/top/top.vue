085f969e818f49f7aee919e2eece58dedb55849b
sgzzy <witzzy@163.com>
2017-04-21 14:04:33 +0800

    update files

--- mweb/mobile-website/src/components/top/top.vue
+++ mweb/mobile-website/src/components/top/top.vue
@@ -1,12 +1,24 @@
 <template>
-  <header><img src='/images/top.png'></header>
+  <header>
+    <img src='./images/top.png'>
+  </header>
 </template>
 
 <script>
-  export defalut {
-
+  export default {
+    name: 'top'
   }
 </script>
 
-<style lang="less" scoped>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  header {
+    width: 100%;
+  }
+
+  header img {
+    display: bolck;
+    width: 100%;
+    height: 100%;
+  }
 </style>
